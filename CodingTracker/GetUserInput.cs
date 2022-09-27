492aa83c2d6d8c63387ced26cc58ba26b9c90c93
Sundebergius <1089441@ucn.dk>
2022-09-27 11:51:26 +0200

    first commit

---
+++ CodingTracker/GetUserInput.cs
@@ -0,0 +1,10 @@
+using System;
+
+internal class GetUserInput
+{
+    CodingController codingController = new();
+    internal void MainMenu()
+    {
+        throw new NotImplementedException();
+    }
+}
