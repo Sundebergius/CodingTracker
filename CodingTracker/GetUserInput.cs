80245affe9e36b45e90c42901e9aa3db7a87aed9
Sundebergius <1089441@ucn.dk>
2022-09-28 09:52:13 +0200

    Scaffolded system with necessary classes/methods

--- CodingTracker/GetUserInput.cs
+++ CodingTracker/GetUserInput.cs
@@ -5,6 +5,35 @@ internal class GetUserInput
     CodingController codingController = new();
     internal void MainMenu()
     {
-        throw new NotImplementedException();
+        
+    }
+
+    private void ProcessAdd()
+    {
+
+    }
+
+    private void ProcessDelete()
+    {
+
+    }
+
+    private void ProcessUpdate()
+    {
+
+    }
+
+    private void ProcessReport()
+    {
+
+    }
+
+    internal string GetDateInput()
+    {
+        Console.WriteLine("\n\nPlease insert the duration: (Format: hh:mm). Type 0 to return to the main menu.\n\n");
+
+        string durationInput = Console.ReadLine();
+        
+        return durationInput;
     }
 }
